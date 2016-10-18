51a5cfdf0737e257fe2f19c21076b78d8ab13a04
jay <jr30149_@hotmail.com>
2016-10-18 19:41:00 +0700

    cccc

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			
 			$messages1 = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "Hello, world"
 				'
 			];
 
@@ -36,12 +36,14 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 			$post = json_encode($data);
+			$post1 = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post1);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
