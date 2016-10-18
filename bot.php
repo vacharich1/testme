1c75c831709b2c57694c9d1df8a788323dc048fa
jay <jr30149_@hotmail.com>
2016-10-18 19:48:45 +0700

    ddddd

--- bot.php
+++ bot.php
@@ -23,11 +23,6 @@ if (!is_null($events['events'])) {
 				
 			];
 			
-			$messages1 = [
-				'type' => 'text',
-				'text' => "Hello, world"
-				'
-			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -36,18 +31,24 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 			$post = json_encode($data);
-			$post1 = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post1);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
+
+			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+			$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+			echo $response->getHTTPStatus() . ' ' . $response->getBody();
 
 			echo $result . "\r\n";
 		}
